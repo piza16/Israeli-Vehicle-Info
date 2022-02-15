b8a4ea5f2aa2f2b432f60fedc5529670dab5911c
piza16 <rotempizanti1@gmail.com>
2022-02-15 21:26:51 +0200

    Add h2 to DynamicTextArea component

--- src/components/DynamicTextArea.tsx
+++ src/components/DynamicTextArea.tsx
@@ -1,7 +1,15 @@
+import React, { useState } from "react"
+
 export const DynamicTextArea = () => {
+    const [textContent,setTextContent]=useState<string>("");
+    
+    const handleChange = (event:React.ChangeEvent<HTMLTextAreaElement>) => {
+        setTextContent(event.currentTarget.value)
+    }
     return(
     <div>
-        <textarea className="mt-4 text-black" name="" id=""></textarea>
+        <textarea onChange={handleChange} className="mt-4 text-black" name="" id=""></textarea>
+        <h2 className="text-white">{textContent}</h2>
     </div>
     )
 }
