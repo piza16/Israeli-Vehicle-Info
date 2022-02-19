5122a38135d37d1a2f1a481bbf0a62ac3d4af48e
piza16 <rotempizanti1@gmail.com>
2022-02-19 17:29:34 +0200

    finnished

--- src/components/ChooseColor.tsx
+++ src/components/ChooseColor.tsx
@@ -2,7 +2,10 @@ export const ChooseColor = (zihum:number):string =>{
     if(zihum>12){
         return "red"
     }
-    else if(zihum>6){
+    else if(zihum>9){
+        return "orange"
+    }
+    else if(zihum>5){
         return "yellow"
     }
     else{
